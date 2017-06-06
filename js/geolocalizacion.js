3dc3a4838b317f21532a3246b27dc3bdbfe12272
Mone Rivas <mone.rivas@gmail.com>
2017-06-06 17:02:15 -0500

    v0.1.0 splash screen completo

---
+++ js/geolocalizacion.js
@@ -0,0 +1,53 @@
+
+$(document).ready(cargarPagina);
+// HOISTING
+function cargarPagina() {
+  obtenerUbicacionActual();
+  $(".ciudad").click(cambiarUbicacion);
+}
+
+function obtenerUbicacionActual() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mostrarPosicionActual);
+  } else {
+    alert("Geolocalización no es soportado en tu navegador");
+  }
+}
+
+function mostrarPosicionActual(posicion) {
+  var latitud = posicion.coords.latitude;
+  var longitud = posicion.coords.longitude;
+
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
+
+  mostrarMapa(coordenadas);
+}
+
+// @coordenadas: { lat: <number>, lng: <number> }
+function mostrarMapa(coordenadas) {
+  var map = new google.maps.Map($('.map')[0], {
+    zoom: 15,
+    center: coordenadas
+  });
+  var marker = new google.maps.Marker({
+    position: coordenadas,
+    map: map
+  });
+}
+
+function cambiarUbicacion() {
+  var latitud = $(this).data("latitud");
+  var longitud = $(this).data("longitud");
+    console.log(this);
+
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
+
+  console.log(coordenadas);
+  mostrarMapa(coordenadas);
+}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(splashScrean);
+
+function splashScrean (){
+    setTimeout(function(){window,location.href="views/home.html";}, 9000);
+
+};
